a0c8e2fdf7fc753e500bcf8db9dfda767020dcd6
snowzhangy <snow_zhangy@hotmail.com>
2019-04-15 11:28:09 -0700

    add faceswap

--- code/demo.py
+++ code/demo.py
@@ -66,6 +66,147 @@ S = np.load('../basics/S.npy')
 MSK = np.reshape(ms_norm, [1, 68*2])
 SK = np.reshape(S, [1, S.shape[0], 68*2])
 
+#...........................................
+#...........................................
+FACE_POINTS = list(range(17, 68))
+MOUTH_POINTS = list(range(48, 61))
+RIGHT_BROW_POINTS = list(range(17, 22))
+LEFT_BROW_POINTS = list(range(22, 27))
+RIGHT_EYE_POINTS = list(range(36, 42))
+LEFT_EYE_POINTS = list(range(42, 48))
+NOSE_POINTS = list(range(27, 35))
+JAW_POINTS = list(range(0, 17))
+# Points used to line up the images.
+ALIGN_POINTS = (LEFT_BROW_POINTS + RIGHT_EYE_POINTS + LEFT_EYE_POINTS +
+                               RIGHT_BROW_POINTS + NOSE_POINTS + MOUTH_POINTS)
+                               # Points from the second image to overlay on the first. The convex hull of each
+# element will be overlaid.
+OVERLAY_POINTS = [
+    LEFT_EYE_POINTS + RIGHT_EYE_POINTS + LEFT_BROW_POINTS + RIGHT_BROW_POINTS,
+    NOSE_POINTS + MOUTH_POINTS,
+]
+COLOUR_CORRECT_BLUR_FRAC = 0.6
+SCALE_FACTOR = 1 
+FEATHER_AMOUNT = 11
+def get_landmarks(im):
+    #gray = cv2.cvtColor(im, cv2.COLOR_BGR2RGB)
+    rects = detector(im, 1)
+    rect=rects[0]
+    if len(rects) > 1:
+        #raise TooManyFaces
+        rect=rects[1]
+    if len(rects) == 0:
+        raise NoFaces
+    return np.matrix([[p.x, p.y] for p in predictor(im, rect).parts()])
+    
+def read_im_and_landmarks(im):
+    im = cv2.resize(im, (im.shape[1] * SCALE_FACTOR,im.shape[0] * SCALE_FACTOR))
+    s = get_landmarks(im)
+    return im, s
+
+def transformation_from_points(points1, points2):
+    """
+    Return an affine transformation [s * R | T] such that:
+
+        sum ||s*R*p1,i + T - p2,i||^2
+
+    is minimized.
+
+    """
+    # Solve the procrustes problem by subtracting centroids, scaling by the
+    # standard deviation, and then using the SVD to calculate the rotation. See
+    # the following for more details:
+    #   https://en.wikipedia.org/wiki/Orthogonal_Procrustes_problem
+
+    points1 = points1.astype(np.float64)
+    points2 = points2.astype(np.float64)
+
+    c1 = np.mean(points1, axis=0)
+    c2 = np.mean(points2, axis=0)
+    points1 -= c1
+    points2 -= c2
+
+    s1 = np.std(points1)
+    s2 = np.std(points2)
+    points1 /= s1
+    points2 /= s2
+
+    U, S, Vt = np.linalg.svd(points1.T * points2)
+
+    # The R we seek is in fact the transpose of the one given by U * Vt. This
+    # is because the above formulation assumes the matrix goes on the right
+    # (with row vectors) where as our solution requires the matrix to be on the
+    # left (with column vectors).
+    R = (U * Vt).T
+
+    return np.vstack([np.hstack(((s2 / s1) * R,
+                                       c2.T - (s2 / s1) * R * c1.T)),
+                         np.matrix([0., 0., 1.])])
+                         
+def draw_convex_hull(im, points, color):
+    points = cv2.convexHull(points)
+    cv2.fillConvexPoly(im, points, color=color)
+def warp_im(im, M, dshape):
+    output_im = np.zeros(dshape, dtype=im.dtype)
+    cv2.warpAffine(im,
+                   M[:2],
+                   (dshape[1], dshape[0]),
+                   dst=output_im,
+                   borderMode=cv2.BORDER_TRANSPARENT,
+                   flags=cv2.WARP_INVERSE_MAP)
+    return output_im
+def get_face_mask(im, landmarks):
+    im = np.zeros(im.shape[:2], dtype=np.float64)
+
+    for group in OVERLAY_POINTS:
+        draw_convex_hull(im,
+                         landmarks[group],
+                         color=1)
+
+    im = np.array([im, im, im]).transpose((1, 2, 0))
+
+    im = (cv2.GaussianBlur(im, (FEATHER_AMOUNT, FEATHER_AMOUNT), 0) > 0) * 1.0
+    im = cv2.GaussianBlur(im, (FEATHER_AMOUNT, FEATHER_AMOUNT), 0)
+
+    return im
+def correct_colours(im1, im2, landmarks1):
+    blur_amount = COLOUR_CORRECT_BLUR_FRAC * np.linalg.norm(
+                              np.mean(landmarks1[LEFT_EYE_POINTS], axis=0) -
+                              np.mean(landmarks1[RIGHT_EYE_POINTS], axis=0))
+    blur_amount = int(blur_amount)
+    if blur_amount % 2 == 0:
+        blur_amount += 1
+    im1_blur = cv2.GaussianBlur(im1, (blur_amount, blur_amount), 0)
+    im2_blur = cv2.GaussianBlur(im2, (blur_amount, blur_amount), 0)
+
+    # Avoid divide-by-zero errors.
+    im2_blur += (128 * (im2_blur <= 1.0)).astype(im2_blur.dtype)
+
+    return (im2.astype(np.float64) * im1_blur.astype(np.float64) /
+                                                im2_blur.astype(np.float64))
+def restore_image(orgImage,rect, img):
+    #(x, y, w, h) = utils.rect_to_bb(rect)
+    cv2.normalize(img, img, 0, 255, cv2.NORM_MINMAX)
+    #img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+    #rgb = cv2.resize(img,(w,h),interpolation=cv2.INTER_CUBIC)
+    #orgImage[y:y + h, x:x+w]=rgb
+
+    im1, landmarks1 = read_im_and_landmarks(orgImage)
+    img = img.astype('uint8')
+    im2, landmarks2 = read_im_and_landmarks(img)
+
+    M = transformation_from_points(landmarks1[ALIGN_POINTS],landmarks2[ALIGN_POINTS])
+    mask = get_face_mask(im2, landmarks2)
+    warped_mask = warp_im(mask, M, im1.shape)
+    combined_mask = np.max([get_face_mask(im1, landmarks1), warped_mask],axis=0)
+    warped_im2 = warp_im(im2, M, im1.shape)
+    warped_corrected_im2 = correct_colours(im1, warped_im2, landmarks1)
+
+    output_im = im1 * (1.0 - combined_mask) + warped_corrected_im2 * combined_mask
+    return output_im
+#...........................................
+#...........................................
+
 def multi2single(model_path, id):
     checkpoint = torch.load(model_path)
     state_dict = checkpoint
@@ -109,14 +250,6 @@ def getImageInfo(image_path):
     #for (i, rect) in enumerate(rects):
     #(x, y, w, h) = utils.rect_to_bb(rect)
     return image,rects[config.faceid]
-        
-def restore_image(orgImage,rect, img):
-    (x, y, w, h) = utils.rect_to_bb(rect)
-    cv2.normalize(img, img, 0, 255, cv2.NORM_MINMAX)
-    img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
-    rgb = cv2.resize(img,(w,h),interpolation=cv2.INTER_CUBIC)
-    orgImage[y:y + h, x:x+w]=rgb
-    return orgImage
  
 def crop_image2(image,rect):
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
